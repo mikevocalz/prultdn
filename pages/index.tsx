633217bc6090fdd23f231ce6639514449f61c405
mikevocalz <mikevocalz@aol.com>
2020-01-19 12:20:45 -0500

    add tsconfig

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'; 
+
+interface Props {
+  
+}
+
+const Index: React.FC<Props> = () => {
+  return (
+    <h1>Hello Next.js ttty 👋</h1>
+  );
+}
+
+
+
+export default Index
+
+ 
